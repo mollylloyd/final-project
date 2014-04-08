5a24632b691ca22bc128c0c959df559244dcb653
mollylloyd <mollylloyd@g.harvard.edu>
2014-04-07 22:36:46 -0400

    wave height json

---
+++ waveHeight.js
@@ -0,0 +1,27 @@
+var jsonRows ={};
+
+d3.csv("../data/waveHeight.csv", function(data) {
+
+	data.forEach(function(d,i){
+	
+	// return {
+	// 	country: d.Country,
+	// 	state: d.State,
+	// 	name: d.Name,
+	// 	distance: d.DistanceFromSource,
+	// 	lat: +d.Latitude,
+	// 	lon: +d.Longitude,
+	// 	waveHeight: +d.MaxWaveHeight,
+	// 	arrivalDate: new Date(2011,03,+d.ArrivalDay,+d.ArrivalHour, +d.ArrivalMin),
+	// 	maxWaveDate: new Date(2011,03,+d.MaxWaveDay,+d.MaxWaveHour,+d.MaxWaveMin)
+	// },
+
+	jsonRows[i] = {"country":d.Country,"state":d.State,"name":d.Name,"distance":+d.DistanceFromSource,"lat":+d.Latitude, "lon":+d.Longitude,
+					"waveHeight":+d.MaxWaveHeight, "waveHeight":+d.MaxWaveHeight,"arrivalDate":  new Date(2011,03,+d.ArrivalDay,+d.ArrivalHour, +d.ArrivalMin),
+					"maxWaveDate":new Date(2011,03,+d.MaxWaveDay,+d.MaxWaveHour,+d.MaxWaveMin)};	
+
+
+	});
+	localStorage.setItem('storage', JSON.stringify(jsonRows))
+});
+
